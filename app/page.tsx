9ba6dfa713e29af6b13f402bc99c90678a114429
Sam Mehra <SamMehra@anyas-mbp-2.lan>
2025-05-31 19:40:37 -0700

    restarting main

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,34 @@ import Image from "next/image";
 export default function Home() {
   return (
 
-<div>
-  <h1 className="text-4xl font-bold text-center font-serif">
-    welcome to hell
-  </h1>
-</div>
+// {/* <div>
+//   <h1 className="text-4xl font-bold text-center font-serif">
+//     welcome to horror club
+//   </h1>
+//   <p>
+//     boooooo
+//   </p>
+// </div> */}
+
+<main className="h-screen snap-y snap-mandatory overflow-y-scroll">
+  {/* header/intro */}
+  <section className="h-screen snap-start flex flex-col items-center justify-center bg-black text-white">
+    <h1 className="text-4xl font-bold font-lucida">
+      horror club 👻
+    </h1>
+  </section>
+
+  {/* movies we've watched */}
+  <section>
+    
+  </section>
+
+  {/* top ten horror recs */}
+  <section>
+    
+  </section>
+
+  {/* about us */}
+</main>
   );
 }
